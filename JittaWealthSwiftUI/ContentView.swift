2d5f10d81496936baa6d6fd0dc35aa8abd776e7c
terminox <art@Arts-MacBook-Pro.local>
2020-01-25 19:31:23 +0700

    Initial commit

---
+++ JittaWealthSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JittaWealthSwiftUI
+//
+//  Created by Art on 25/1/2563 BE.
+//  Copyright © 2563 Art. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
